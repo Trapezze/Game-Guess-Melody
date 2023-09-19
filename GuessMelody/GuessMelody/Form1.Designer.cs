4d78ae99918cd45c081c2381bfede0f9cb6b6a60
Mark Myslevets <36281997+Trapezze@users.noreply.github.com>
2023-09-19 20:45:26 +1000

    Add files via upload

---
+++ GuessMelody/GuessMelody/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace GuessMelody
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnParam = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(472, 63);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(159, 96);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Game";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // btnParam
+            // 
+            this.btnParam.Location = new System.Drawing.Point(472, 192);
+            this.btnParam.Name = "btnParam";
+            this.btnParam.Size = new System.Drawing.Size(159, 96);
+            this.btnParam.TabIndex = 1;
+            this.btnParam.Text = "Settings";
+            this.btnParam.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(472, 332);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(159, 96);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 550);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnParam);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "fMain";
+            this.Text = "Guess Melody";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnParam;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
